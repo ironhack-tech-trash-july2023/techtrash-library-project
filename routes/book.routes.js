8408d4c13dde0a7364628aeb4e712a4432d2943e
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-08-29 14:46:07 +0200

    fix: functionality to create new book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -26,7 +26,17 @@ router.get("/books", (req, res, next) => {
 
 // CREATE: display form
 router.get("/books/create", (req, res, next) => {
-    res.render("books/book-create");
+    Author.find()
+        .then( authorsFromDB => {
+            const data = {
+                authors: authorsFromDB
+            }
+            res.render("books/book-create", data);
+        })
+        .catch((e) => {
+            console.log("Error getting list of authors from DB", e);
+            next(e);
+        });
 });
 
 
